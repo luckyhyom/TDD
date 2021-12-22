a2efcee6fff82835a148a8aeac8b453eacf443fd
luckyhyom <bs_khm@naver.com>
2021-12-22 14:43:41 +0900

    [Test] 계산기 테스트코드 1차 수정
    - describe를 이용하여 연관된 테스트코드 묶기
    - beforeEach를 이용하여 각 테스트코드에서 인스턴스 중복코드 제거
      - describe에서 Calculaltor의 테스트코드임을 명시하고 있으므로.. 테스트 주체가 되는 클래스를 선언하는 과정은 beforeEach를 사용해서 생략해도 되지않을까?
    
    etc.
    - 0/0, 10/0 의 결과

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,60 +1,71 @@
 const Calculator = require('../src/calculator');
 
 
-test('defines value', () => {
-    const calculator = new Calculator();
-    expect(calculator.value).toBe(0);
-
-    calculator.set(10);
-    expect(calculator.value).toBe(10);
-
-    calculator.set(5);
-    expect(calculator.value).toBe(5);
-})
-
-test('clear', () => {
-    const calculator = new Calculator();
-    calculator.set(10);
-    expect(calculator.value).toBe(10);
-
-    calculator.clear();
-    expect(calculator.value).toBe(0);
-})
-
-describe('add', () => {
-    test('add', () => {
-        const calculator = new Calculator();
-        calculator.add(1);
-        expect(calculator.value).toBe(1);
-        calculator.add(10);
-        expect(calculator.value).toBe(11);
+describe('Calculator', () => {
+    let calculator;
+    beforeEach(() => {
+        calculator = new Calculator();
     })
-
-    test('can`t be bigger than 100', () => {
-            const calculator = new Calculator();
-            calculator.add(100);
-            expect(calculator.value).toBe(100);
-            expect(() => calculator.add(1)).toThrow('Value can not be greater than 100');
+    it('defines value', () => {
+        expect(calculator.value).toBe(0);
+    
+        calculator.set(10);
+        expect(calculator.value).toBe(10);
+    
+        calculator.set(5);
+        expect(calculator.value).toBe(5);
     })
-})
-
-test('subtract', () => {
-    const calculator = new Calculator();
-    expect(calculator.value).toBe(0);
-    calculator.subtract(10);
-    expect(calculator.value).toBe(-10);
-})
-
-test('multiply', () => {
-    const calculator = new Calculator();
-    calculator.set(10);
-    calculator.multiply(2)
-    expect(calculator.value).toBe(20);
-})
-
-test('multiply', () => {
-    const calculator = new Calculator();
-    calculator.set(10);
-    calculator.divide(2)
-    expect(calculator.value).toBe(5);
+    
+    it('clear', () => {
+        calculator.set(10);
+        expect(calculator.value).toBe(10);
+    
+        calculator.clear();
+        expect(calculator.value).toBe(0);
+    })
+    
+    describe('add', () => {
+        it('add', () => {
+            calculator.add(1);
+            expect(calculator.value).toBe(1);
+            calculator.add(10);
+            expect(calculator.value).toBe(11);
+        })
+    
+        it('can`t be bigger than 100', () => {
+                calculator.add(100);
+                expect(calculator.value).toBe(100);
+                expect(() => calculator.add(1)).toThrow('Value can not be greater than 100');
+        })
+    })
+    
+    it('subtract', () => {
+        expect(calculator.value).toBe(0);
+        calculator.subtract(10);
+        expect(calculator.value).toBe(-10);
+    })
+    
+    it('multiply', () => {
+        calculator.set(10);
+        calculator.multiply(2)
+        expect(calculator.value).toBe(20);
+    })
+    
+    describe('devide', () => {
+        it('0 / 0 === NaN', () => {
+            calculator.set(0);
+            calculator.divide(0);
+            expect(calculator.value).toBe(NaN);
+        })
+        it('10 / 0 === Infinity', () => {
+            calculator.set(10);
+            calculator.divide(0);
+            expect(calculator.value).toBe(Infinity);
+        })
+        it('10 / 2 === 5', () => {
+            calculator.set(10);
+            calculator.divide(2)
+            expect(calculator.value).toBe(5);
+        })
+    })
 })
