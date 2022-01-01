668e4e41650d7c8f4de0836787df437b1aea0a7e
luckyhyom <bs_khm@naver.com>
2022-01-01 23:58:06 +0900

    [Test] login 테스트코드 작성

--- src/mock/user/test/user_service.test.js
+++ src/mock/user/test/user_service.test.js
@@ -1,27 +1,36 @@
 const UserService = require('../user_service');
 const UserClient = require('../user_client');
 
-jest.mock('../user_client');
+jest.mock('../user_client.js');
 describe('', () => {
-    beforeAll(() => {
-        UserClient.mockImplementation(() => {
-            return {
-                login: jest.fn(async () => {
-                    return {
-                        data: true
-                    }
-                })
-            }
-        })
+    const login = jest.fn(async () => {
+        return { data: true }
+    })
+    UserClient.mockImplementation(() => {
+        return {
+            login
+        }
     })
 
     let service;
     beforeEach(() => {
-        service = new UserService();
+        service = new UserService(new UserClient());
     })
     it('로그인 상태를 가지고 있다.', async () => {
         expect(service.isLogedIn).toBeFalsy();
-        service.login('id', 'password');
-        // expect(UserClient.mock.login).toBeCalledTimes(1);
+        await service.login('id', 'password');
+        expect(login).toBeCalledTimes(1);
+    });
+    
+    it('로그인 상태라면 login 메소드를 호출하지 않는다.', async () => {
+        await service.login('id', 'password');
+        expect(service.isLogedIn).toBeTruthy();
+        expect(login).toBeCalledTimes(1);
+
+        await service.login('id', 'password');
+        await service.login('id', 'password');
+        await service.login('id', 'password');
+        await service.login('id', 'password');
+        expect(login).toBeCalledTimes(1);
     });
 })
